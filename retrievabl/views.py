4a95008beb43640b742cfd1b26cdcc2a22633027
KaitlynRandolph <knr0022@auburn.edu>
2020-03-26 14:14:37 -0500

    improved design of base_page

--- retrievabl/views.py
+++ retrievabl/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'retrievabl/index.html')
+
+
+def search(request):
+    return render(request, 'retrievabl/base_page.html')
